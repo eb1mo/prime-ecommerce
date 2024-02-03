1718bf2918aaa0b70469667300c66eb757eb14ad
Sushil Balami <sushil.balami200@gmail.com>
2024-02-03 09:20:40 +0545

    ADD: useFetch Hook

---
+++ e-commerce-frontend/src/components/Loading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Loading() {
+  return (
+    <div className="flex items-center justify-center w-full h-36">
+      <span class="loader"></span>
+    </div>
+  );
+}
+
+export default Loading;
